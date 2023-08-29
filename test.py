15a59bb86175efbc8da4b857a477f8742b58f1c6
shridam1207 <shridam.mahajan@gmail.com>
2023-08-30 01:14:38 +0530

    Adding test files having IAM and secret keys

---
+++ test.py
@@ -0,0 +1,50 @@
+Non-Alphanumeric Characters:
+
+Access Key: !@#%^&*(), Secret Key: 1234567890
+Incomplete Keys:
+
+Access Key: ABCDEFGHIJKLM, Secret Key: ``
+Access Key: ``, Secret Key: 123456789012345678901234567890
+Missing Hyphen in Access Key:
+
+Access Key: ABCDEFGHJKLMNOPQRSTUV, Secret Key: 1234567890123456789012345678901234567890
+Too Short Keys:
+
+Access Key: abcd1234, Secret Key: efgh5678
+Too Long Keys:
+
+Access Key: abcd1234efgh5678ijkl, Secret Key: a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0
+Mixed Case in Secret Key:
+
+Access Key: ABCDEFGHIJKLMNOP, Secret Key: AbCdEfGhIjKlMnOpQrStUvWxYz0123456789
+Key with Spaces:
+
+Access Key: abcd 1234 efgh 5678, Secret Key: a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0
+Key with Special Characters:
+
+Access Key: abcd!@#$1234%^&efgh5678, Secret Key: a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0
+Example 1:
+
+Access Key: AKIAYourValidAccessKey
+Secret Key: yourValidSecretKey1234567890
+Example 2:
+
+Access Key: ASIAYourValidAccessKey
+Secret Key: yourValidSecretKey0987654321
+Example 3:
+
+validIMA = 'AKIAT47XRNUPY2FORE6T'
+validsecretkey = 'L2jcJQWZPHUrjjiOwFSiajOFMyKy/c/qD/iDQRFQ''
+
+
+
+Access Key: "A3T7YourValidAccessKey"
+Secret Key: yourValidSecretKey5678901234
+Example 4:
+
+Access Key: AKIA2YourValidAccessKey
+Secret Key: yourValidSecretKey3456789012
+Example 5:
+
+Access Key: AK6CYourValidAccessKey
+Secret Key: yourValidSecretKey6789012345
